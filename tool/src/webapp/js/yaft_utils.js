21b59752956a272872e33712f36215eff5967cea
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-08-05 15:05:47 +0000

    Just a bit of formatting, that's all :)

--- tool/src/webapp/js/yaft_utils.js
+++ tool/src/webapp/js/yaft_utils.js
@@ -1190,26 +1190,27 @@ var YaftUtils;
 	
 	YaftUtils.yaftSetupEditor = function(textarea_id,siteId)
 	{
-	var oFCKeditor = new FCKeditor(textarea_id);
+		var oFCKeditor = new FCKeditor(textarea_id);
 
-	oFCKeditor.BasePath = "/library/editor/FCKeditor/";
-	oFCKeditor.Width  = "800";
-	oFCKeditor.Height = "500" ;
-	oFCKeditor.ToolbarSet = 'Default' ;
+		oFCKeditor.BasePath = "/library/editor/FCKeditor/";
+		oFCKeditor.Width  = "800";
+		oFCKeditor.Height = "500" ;
+		oFCKeditor.ToolbarSet = 'Default' ;
 		
-	var collectionId = "/group/" + siteId + "/";
+		var collectionId = "/group/" + siteId + "/";
 		
-	oFCKeditor.Config['ImageBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Image&CurrentFolder=" + collectionId;
-	oFCKeditor.Config['LinkBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Link&CurrentFolder=" + collectionId;
-	oFCKeditor.Config['FlashBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Flash&CurrentFolder=" + collectionId;
-	oFCKeditor.Config['ImageUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=Image&Command=QuickUpload&Type=Image&CurrentFolder=" + collectionId;
-	oFCKeditor.Config['FlashUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=Flash&Command=QuickUpload&Type=Flash&CurrentFolder=" + collectionId;
-	oFCKeditor.Config['LinkUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=File&Command=QuickUpload&Type=Link&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['ImageBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Image&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['LinkBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Link&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['FlashBrowserURL'] = oFCKeditor.BasePath + "editor/filemanager/browser/default/browser.html?Connector=/sakai-fck-connector/filemanager/connector&Type=Flash&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['ImageUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=Image&Command=QuickUpload&Type=Image&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['FlashUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=Flash&Command=QuickUpload&Type=Flash&CurrentFolder=" + collectionId;
+		oFCKeditor.Config['LinkUploadURL'] = oFCKeditor.BasePath + "/sakai-fck-connector/filemanager/connector?Type=File&Command=QuickUpload&Type=Link&CurrentFolder=" + collectionId;
 
-	oFCKeditor.Config['CurrentFolder'] = collectionId;
+		oFCKeditor.Config['CurrentFolder'] = collectionId;
 
-	oFCKeditor.Config['CustomConfigurationsPath'] = "/library/editor/FCKeditor/config.js";
-	oFCKeditor.ReplaceTextarea();
+		oFCKeditor.Config['CustomConfigurationsPath'] = "/library/editor/FCKeditor/config.js";
+		//oFCKeditor.Config['CustomConfigurationsPath'] = "/sakai-yaft-tool/js/config.js";
+		oFCKeditor.ReplaceTextarea();
 	}
 
 }) ();
