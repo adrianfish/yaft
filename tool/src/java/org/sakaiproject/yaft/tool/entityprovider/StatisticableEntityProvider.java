8d4c760022a684c6b7f2346ec2f0a2d73351b463
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-08-10 14:31:58 +0000

    Changed name of provider to reflect the fact that is just registers events for sitestats

---
+++ tool/src/java/org/sakaiproject/yaft/tool/entityprovider/StatisticableEntityProvider.java
@@ -0,0 +1,74 @@
+package org.sakaiproject.yaft.tool.entityprovider;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Statisticable;
+import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
+import org.sakaiproject.util.ResourceLoader;
+import org.sakaiproject.yaft.api.YaftForumService;
+
+public class StatisticableEntityProvider extends AbstractEntityProvider implements AutoRegisterEntityProvider,Statisticable
+{
+	protected final Logger LOG = Logger.getLogger(getClass());
+	
+	public final static String ENTITY_PREFIX = "yaft-forum";
+	
+	private static final String[] EVENT_KEYS
+		= new String[] {
+			YaftForumService.YAFT_FORUM_CREATED,
+			YaftForumService.YAFT_FORUM_DELETED,
+			YaftForumService.YAFT_DISCUSSION_CREATED,
+			YaftForumService.YAFT_DISCUSSION_DELETED,
+			YaftForumService.YAFT_MESSAGE_CREATED,
+			YaftForumService.YAFT_MESSAGE_DELETED
+			};
+	
+	/*
+	public Object getSampleEntity()
+	{
+		return new Forum();
+	}
+	*/
+
+	public String getEntityPrefix()
+	{
+		return ENTITY_PREFIX;
+	}
+	
+	/**
+	 * From Statisticable
+	 */
+	public String getAssociatedToolId()
+	{
+		return "sakai.yaft";
+	}
+
+	/**
+	 * From Statisticable
+	 */
+	public String[] getEventKeys()
+	{
+		String[] temp = new String[EVENT_KEYS.length];
+		System.arraycopy(EVENT_KEYS, 0, temp, 0, EVENT_KEYS.length);
+		return temp;
+	}
+
+	/**
+	 * From Statisticable
+	 */
+	public Map<String, String> getEventNames(Locale locale)
+	{
+		Map<String, String> localeEventNames = new HashMap<String, String>();
+		ResourceLoader msgs = new ResourceLoader("YaftEvents");
+		msgs.setContextLocale(locale);
+		for (int i = 0; i < EVENT_KEYS.length; i++)
+		{
+			localeEventNames.put(EVENT_KEYS[i], msgs.getString(EVENT_KEYS[i]));
+		}
+		return localeEventNames;
+	}
+}
