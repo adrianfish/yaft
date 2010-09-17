9993d34c782bd53f10403ce662a141ffbdd9ff82
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-09-17 13:26:15 +0000

    Stipped erroneously applied Sakai license

--- tool/src/webapp/lib/jquery.MultiFile.pack.js
+++ tool/src/webapp/lib/jquery.MultiFile.pack.js
@@ -1,19 +1,4 @@
 /*
- * Copyright 2009 The Sakai Foundation
- *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl1.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
  ### jQuery Multiple File Upload Plugin v1.46 - 2009-05-12 ###
  * Home: http://www.fyneworks.com/jquery/multiple-file-upload/
  * Code: http://code.google.com/p/jquery-multifile-plugin/
--- tool/src/webapp/lib/jquery.form.js
+++ tool/src/webapp/lib/jquery.form.js
@@ -1,28 +1,3 @@
-/*
- * Copyright 2009 The Sakai Foundation
- *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl1.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * jQuery Form Plugin
- * version: 2.24 (10-MAR-2009)
- * @requires jQuery v1.2.2 or later
- *
- * Examples and documentation at: http://malsup.com/jquery/form/
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- */
 ;(function($) {
 
 /*
